597d2edd14c0f7a5c3380caee5481a69e0c12c54
asifanwar1 <asifanwar0705@gmail.com>
2026-03-25 01:24:56 +0500

    adding custom components

--- src/components/custom/CustomTooltip/types.ts
+++ src/components/custom/CustomTooltip/types.ts
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react";
 
 export type TCustomTooltipProps = {
-  content: ReactNode;
-  children: ReactNode;
-  className?: string;
-  side?: "top" | "bottom" | "left" | "right";
-  sideOffset?: number;
-  delayDuration?: number;
+    content: ReactNode;
+    children: ReactNode;
+    className?: string;
+    side?: "top" | "bottom" | "left" | "right";
+    sideOffset?: number;
+    delayDuration?: number;
 };
