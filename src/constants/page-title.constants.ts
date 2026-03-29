4386bd191f19cfbf2975ec416adf965f84dd7e1d
asifanwar1 <asifanwar0705@gmail.com>
2026-03-30 04:32:45 +0500

    Topbar setup

--- src/constants/page-title.constants.ts
+++ src/constants/page-title.constants.ts
@@ -9,44 +9,10 @@ export const defaultRouteNames: Record<string, string> = {
     [APP_ROUTES.CUSTOMER_MANAGEMENT]: "Customer Management",
     [APP_ROUTES.CUSTOMER_MANAGEMENT_DETAILS]: "Customer Management",
 
-    // Installer Management
-    [APP_ROUTES.INSTALLER_MANAGEMENT]: "Installer Management",
-    [APP_ROUTES.INSTALLER_MANAGEMENT_DETAILS]: "Installer Management",
-
-    // Preset Management
-    [APP_ROUTES.PRESET_MANAGEMENT]: "Preset Management",
-    [APP_ROUTES.ADD_PRESET]: "Preset Management",
-    [APP_ROUTES.EDIT_PRESET]: "Preset Management",
-    [APP_ROUTES.PRESET_DETAILS]: "Preset Management",
-
-    // Portfolio Management
-    [APP_ROUTES.PORTFOLIO_MANAGEMENT]: "Portfolio Management",
-    [APP_ROUTES.ADD_PORTFOLIO]: "Portfolio Management",
-    [APP_ROUTES.EDIT_PORTFOLIO]: "Portfolio Management",
-    [APP_ROUTES.PORTFOLIO_DETAILS]: "Portfolio Management",
-
-    // Reviews Management
-    [APP_ROUTES.REVIEWS_MANAGEMENT]: "Reviews Management",
-    [APP_ROUTES.REVIEW_MANAGEMENT]: "Review Management",
-
     // Account Management
     [APP_ROUTES.ACCOUNT]: "Account",
     [APP_ROUTES.ACCOUNT_PROFILE]: "Profile",
     [APP_ROUTES.ACCOUNT_CHANGE_PASSWORD]: "Change Password",
-
-    // Quote Management
-    [APP_ROUTES.QUOTE_MANAGEMENT]: "Quote Management",
-    [APP_ROUTES.QUOTE_MANAGEMENT_DETAILS]: "Quote Management",
-
-    // Deals Management
-    [APP_ROUTES.DEALS_MANAGEMENT]: "Deals Management",
-    [APP_ROUTES.DEALS_MANAGEMENT_DETAILS]: "Deals Management",
-    [APP_ROUTES.DEALS_MANAGEMENT_EDIT]: "Deals Management",
-    [APP_ROUTES.DEALS_MANAGEMENT_ADD]: "Deals Management",
-
-    // Renovation Management
-    [APP_ROUTES.RENOVATION_MANAGEMENT]: "Renovation Management",
-    [APP_ROUTES.RENOVATION_MANAGEMENT_DETAILS]: "Renovation Management",
 
     // Authentication Routes
     [APP_ROUTES.LOGIN]: "Login",
