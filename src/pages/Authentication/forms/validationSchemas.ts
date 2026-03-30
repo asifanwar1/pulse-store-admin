ea7db2786573bda09e23daf58139b5a62e5fd387
asifanwar1 <asifanwar0705@gmail.com>
2026-03-31 02:00:28 +0500

    auth module ui implementation

--- src/pages/Authentication/forms/validationSchemas.ts
+++ src/pages/Authentication/forms/validationSchemas.ts
@@ -2,39 +2,40 @@ import { z } from "zod";
 
 export const LOGIN_FORM_SCHEMA = z.object({
     email: z
-        .string({ required_error: "Email is required" })
+        .string()
+        .min(1, "Email is required")
         .email("Please enter a valid email address"),
-    password: z
-        .string({ required_error: "Password is required" })
-        .min(6, "Password must be at least 6 characters long"),
-    type: z.string({ required_error: "Type is required" }),
-    rememberToken: z.boolean().optional()
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    type: z.string().min(1, "Type is required"),
+    rememberToken: z.boolean().optional(),
 });
 
 export const FORGOT_PASSWORD_FORM_SCHEMA = z.object({
     email: z
-        .string({ required_error: "Email is required" })
+        .string()
+        .min(1, "Email is required")
         .email("Please enter a valid email address"),
-    type: z.string({ required_error: "Type is required" })
+    type: z.string().min(1, "Type is required"),
 });
 
 export const FORGOT_PASSWORD_VERIFICATION_SCHEMA = z.object({
     code: z
-        .string({ required_error: "Verification code is required" })
+        .string()
+        .min(1, "Verification code is required")
         .length(4, "Verification code must be 4 digits")
-        .regex(/^\d{4}$/, "Verification code must be numeric")
+        .regex(/^\d{4}$/, "Verification code must be numeric"),
 });
 
 export const RESET_PASSWORD_FORM_SCHEMA = z
     .object({
         password: z
-            .string({ required_error: "Password is required" })
+            .string()
             .min(6, "Password must be at least 6 characters long"),
         confirmPassword: z
-            .string({ required_error: "Please confirm your password" })
-            .min(6, "Confirm Password must be at least 6 characters long")
+            .string()
+            .min(6, "Confirm Password must be at least 6 characters long"),
     })
     .refine((data) => data.password === data.confirmPassword, {
         message: "Passwords do not match",
-        path: ["confirmPassword"]
+        path: ["confirmPassword"],
     });
