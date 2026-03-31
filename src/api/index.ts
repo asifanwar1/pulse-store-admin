49d196433c7099fa1ec8d02fa9d3f6bdabf67513
asifanwar1 <asifanwar0705@gmail.com>
2026-03-31 17:15:10 +0500

    resolved type errors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,3 @@
 export * from "./services/auth";
-export * from "./services/media";
-export * from "./services/product";
 export * from "./services/users";
-export * from "./services/projects";
-export * from "./services/portfolio";
 export * from "./services/dashboard";
