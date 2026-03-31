fe6cd6303c7ffded674ac9b7e1d4b0d5cc571f04
asifanwar1 <asifanwar0705@gmail.com>
2026-03-31 16:23:13 +0500

    fixing assets and logos

--- src/layouts/menu.config.ts
+++ src/layouts/menu.config.ts
@@ -1,13 +1,5 @@
 import { APP_ROUTES } from "@/routes/appRoutes";
 
-import dashboardIconActive from "@/assets/icons/dashboard-icon.svg";
-import dashboardIconInactive from "@/assets/icons/dashboard-icon-inactive.svg";
-import customerIconActive from "@/assets/icons/customers-icon-active.svg";
-import customerIconInactive from "@/assets/icons/customers-icon.svg";
-
-import productIconActive from "@/assets/icons/product-icon-active.svg";
-import productIconInactive from "@/assets/icons/product-icon.svg";
-
 export type SidebarContainerType = {
     label: string;
     inactiveIcon: string;
@@ -17,21 +9,21 @@ export type SidebarContainerType = {
 export const menuItems: Array<SidebarContainerType> = [
     {
         label: "Dashboard",
-        inactiveIcon: dashboardIconInactive,
-        activeIcon: dashboardIconActive,
+        inactiveIcon: "",
+        activeIcon: "",
         path: [APP_ROUTES.ROOT, APP_ROUTES.DASHBOARD],
     },
     {
         label: "Customer Management",
-        inactiveIcon: customerIconInactive,
-        activeIcon: customerIconActive,
+        inactiveIcon: "",
+        activeIcon: "",
         path: [APP_ROUTES.CUSTOMER_MANAGEMENT],
     },
 
     {
         label: "Product Management",
-        inactiveIcon: productIconInactive,
-        activeIcon: productIconActive,
+        inactiveIcon: "",
+        activeIcon: "",
         path: [APP_ROUTES.DASHBOARD],
     },
 ];
