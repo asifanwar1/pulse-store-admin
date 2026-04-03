f872a415f44dc33a012e39f4918e1ba5fa95faef
asifanwar1 <asifanwar0705@gmail.com>
2026-04-04 03:44:45 +0500

    routes fix

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,24 +1,24 @@
-import React, { Suspense, useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 
 // import { useStore } from "@/store";
-import { APP_ROUTES } from "@/routes/appRoutes";
+// import { APP_ROUTES } from "@/routes/appRoutes";
 import Loading from "@/components/custom/Loading";
 
 const AuthLayout: React.FC = () => {
     // const isAuthenticated = useStore((state) => state.isAuthenticated);
-    const isAuthenticated = true;
-    const navigate = useNavigate();
+    // const isAuthenticated = true;
+    // const navigate = useNavigate();
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            navigate(APP_ROUTES.DASHBOARD);
-        }
-    }, [isAuthenticated, navigate]);
+    // useEffect(() => {
+    //     if (isAuthenticated) {
+    //         navigate(APP_ROUTES.DASHBOARD);
+    //     }
+    // }, [isAuthenticated, navigate]);
 
-    if (isAuthenticated) {
-        return null;
-    }
+    // if (isAuthenticated) {
+    //     return null;
+    // }
 
     return (
         <Suspense fallback={<Loading />}>
