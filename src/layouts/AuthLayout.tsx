8ab16c4747edd0155dbe16865b95e95f57c5f8bc
asifanwar1 <asifanwar0705@gmail.com>
2026-04-03 10:00:00 +0500

    routes fix

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
-import { useStore } from "@/store";
+// import { useStore } from "@/store";
 import { APP_ROUTES } from "@/routes/appRoutes";
 import Loading from "@/components/custom/Loading";
 
 const AuthLayout: React.FC = () => {
-    const isAuthenticated = useStore((state) => state.isAuthenticated);
+    // const isAuthenticated = useStore((state) => state.isAuthenticated);
+    const isAuthenticated = true;
     const navigate = useNavigate();
 
     useEffect(() => {
