f3b42503fd9cc1b8a150027db87ed6328e5eb94f
asifanwar1 <asifanwar0705@gmail.com>
2026-05-25 18:38:36 +0500

    fixes in products

--- src/pages/ProductManagement/ProductManagement.Container.ts
+++ src/pages/ProductManagement/ProductManagement.Container.ts
@@ -1,4 +1,35 @@
+import Config from "@/Config";
+import { useGetProducts } from "@/hooks/api/products.queries";
+import { useQueryState } from "nuqs";
+
 export const useProductManagement = () => {
-    // Placeholder for any future logic related to product management
-    return {};
+    const [search, setSearch] = useQueryState("search", { defaultValue: "" });
+    const [pageSize, setPageSize] = useQueryState("pageSize", {
+        defaultValue: Config.LIMIT,
+        parse: Number,
+        serialize: String,
+    });
+
+    const {
+        data: products,
+        count: productsTotalCount,
+        isPending: isProductsLoading,
+        page,
+        setPage,
+    } = useGetProducts({
+        search,
+        page: 1,
+        limit: pageSize,
+    });
+    return {
+        products,
+        productsTotalCount,
+        isProductsLoading,
+        page,
+        search,
+        pageSize,
+        setPage,
+        setSearch,
+        setPageSize,
+    };
 };
