94afa5607fa6c7b1e293809464a4160a42898b70
asifanwar1 <asifanwar0705@gmail.com>
2026-06-12 10:00:00 +0500

    fixes

--- src/pages/ShipmentManagement/ShipmentManagement.Container.ts
+++ src/pages/ShipmentManagement/ShipmentManagement.Container.ts
@@ -1,3 +1,17 @@
+import { useGetShipments } from "@/hooks/api/shipment.queries";
+
 export const useShipmentManagement = () => {
-    return {};
+    const {
+        data: shipments,
+        count: shipmentsTotalCount,
+        isPending: isShipmentsDataLoading,
+        page,
+        setPage,
+    } = useGetShipments({
+        search,
+        page: 1,
+        limit: pageSize,
+    });
+
+    return { shipments, shipmentsTotalCountm, isShipmentsDataLoading };
 };
