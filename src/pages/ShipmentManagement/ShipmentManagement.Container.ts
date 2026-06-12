57f68419d526ec6c5952976650b3a83d86d5840f
asifanwar1 <asifanwar0705@gmail.com>
2026-06-12 10:00:00 +0500

    fixes

--- src/pages/ShipmentManagement/ShipmentManagement.Container.ts
+++ src/pages/ShipmentManagement/ShipmentManagement.Container.ts
@@ -1,6 +1,14 @@
 import { useGetShipments } from "@/hooks/api/shipment.queries";
+import { useQueryState } from "nuqs";
 
 export const useShipmentManagement = () => {
+    const [search, setSearch] = useQueryState("search", { defaultValue: "" });
+    const [pageSize, setPageSize] = useQueryState("pageSize", {
+        defaultValue: Config.LIMIT,
+        parse: Number,
+        serialize: String,
+    });
+
     const {
         data: shipments,
         count: shipmentsTotalCount,
