6768e6bde71742a1a8c2ebaaeaf1eedc23130ffc
asifanwar1 <asifanwar0705@gmail.com>
2026-08-15 12:11:19 +0500

    code refactoring

--- src/components/custom/DataTable/index.ts
+++ src/components/custom/DataTable/index.ts
@@ -1,8 +1,5 @@
-// Main component
 export { default as DataTable } from "./DataTable";
 export { default as TableToolbar } from "./TableToolbar";
-
-// Components
 
 export { default as DataTableContent } from "./TableComponents/DataTableContent";
 export { default as DataTablePagination } from "./TableComponents/DataTablePagination";
@@ -13,25 +10,19 @@ export { default as DataTableEmpty } from "./TableComponents/DataTableEmpty";
 export { default as DataTableError } from "./TableComponents/DataTableError";
 export { default as SortableHeader } from "./TableComponents/SortableHeader";
 
-
-// Types
 export type {
-  IDataTableConfig,
-  IDataTableProps,
-  TDataColumnDef,
-  IDataColumnMeta,
-  TFilterVariant,
-  // FilterOption,
-  TFilterOperator,
-  IFilterConfig,
-  IDataTableFeatures,
-  IDataTableStyling,
-  IDataTableCallbacks,
-  IDataTableInitialState,
- 
-  IRowAction,
-  IExportConfig,
-  IDataTableState,
+    IDataTableConfig,
+    IDataTableProps,
+    TDataColumnDef,
+    IDataColumnMeta,
+    TFilterVariant,
+    TFilterOperator,
+    IFilterConfig,
+    IDataTableFeatures,
+    IDataTableStyling,
+    IDataTableCallbacks,
+    IDataTableInitialState,
+    IRowAction,
+    IExportConfig,
+    IDataTableState,
 } from "./TableTypes";
-
-
